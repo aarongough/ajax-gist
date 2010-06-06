33ebc3de1a69949d368b4b182a27bff5a3abcc59
Aaron Gough <aaron@aarongough.com>
2010-06-05 22:01:08 -0400

    Basic version of AjaxGist model up and running

--- test/unit/ajax_gist_test.rb
+++ test/unit/ajax_gist_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class AjaxGistTest < ActiveSupport::TestCase
   
+  def setup
+    github_gist_stubs
+  end
+  
   test "AjaxGist.base_url should return base gist URL" do
-    assert_equal "http://gist.github.com", AjaxGist.base_url
+    assert_equal "http://gist.github.com/", AjaxGist.base_url
   end
   
   test "AjaxGist.gist_embed_url_for_id should raise error if not supplied with an id" do
@@ -13,29 +17,30 @@ class AjaxGistTest < ActiveSupport::TestCase
   end
   
   test "AjaxGist.gist_embed_url_for_id should return gist embed url" do
-    assert_equal "http://gist.github.com/101010.js", AjaxGist.gist_embed_url_for_id(1010)
+    assert_equal "http://gist.github.com/1010.js", AjaxGist.gist_embed_url_for_id(1010)
   end
   
-  test "AjaxGist.fetch_gist_embed_code should raise error if not supplied with a URL" do
+  test "AjaxGist.gist_embed_code should raise error if not supplied with a URL" do
     assert_raises ArgumentError do
-     AjaxGist.fetch_gist_embed_code
+     AjaxGist.gist_embed_code
     end
   end
   
-  test "AjaxGist.fetch_gist_embed_code should return Gist embed code" do
-    gist = AjaxGist.fetch_gist_embed_code(1010)
+  test "AjaxGist.gist_embed_code should return Gist embed code" do
+    gist = AjaxGist.gist_embed_code(1010)
     assert gist.include?("document.write"), "Gist embed code did not include 'document.write'"
   end
   
-  test "AjaxGist.ajax_gist_embed_code should raise error if not supplied with id" do
+  test "AjaxGist.gist_markup_for should raise error if not supplied with id" do
     assert_raises ArgumentError do
-      AjaxGist.ajax_gist_embed_code
+      AjaxGist.gist_markup_for
     end
   end
   
-  test "AjaxGist.ajax_gist_embed_code should return treated gist embed code" do
-    gist = AjaxGist.ajax_gist_embed_code(1010)
-    assert gist.include?("document.createElement"), "Treated Gist embed code did not include 'document.createElement'"
+  test "AjaxGist.gist_markup_for should return treated gist embed code" do
+    gist = AjaxGist.gist_markup_for(1010)
+    assert gist.include?('<div id=\"gist-'), "Gist markup did not include opening div"
+    assert !gist.include?('document.write('), "Gist markup should not include document.write"
   end
   
 end
