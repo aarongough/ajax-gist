78a265e7bef659eaa5812899f15b4d57b3930816
Aaron Gough <aaron@aarongough.com>
2010-06-06 02:28:05 -0400

    Added basic tests for info_controller

--- test/functional/info_controller_test.rb
+++ test/functional/info_controller_test.rb
@@ -1,8 +1,45 @@
 require 'test_helper'
 
 class InfoControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "index should return success" do
+    get :index
+    assert_response :success
   end
+  
+  test "index should use index template" do
+    get :index
+    assert_template :index
+  end
+  
+  test "overview should return success" do
+    get :overview
+    assert_response :success
+  end
+  
+  test "overview should use index template" do
+    get :overview
+    assert_template :overview
+  end
+  
+  test "static should return success" do
+    get :static
+    assert_response :success
+  end
+  
+  test "static should use index template" do
+    get :static
+    assert_template :static
+  end
+  
+  test "ajax should return success" do
+    get :ajax
+    assert_response :success
+  end
+  
+  test "ajax should use index template" do
+    get :ajax
+    assert_template :ajax
+  end
+  
 end
