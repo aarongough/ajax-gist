d239c42d17126c3f1952b2015156a182c5b88a2f
Aaron Gough <aaron@aarongough.com>
2010-06-05 23:33:45 -0400

    Modified performance test to stub out network calls, helps isolate application code

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -3,6 +3,11 @@ require 'performance_test_help'
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionController::PerformanceTest
+
+  def setup
+    github_gist_stubs
+  end
+
   def test_gist
     get '/gist/1010.js'
   end
