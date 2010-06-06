f14ff218a4b18c52d7be111a1ad2f1e08aabe42e
Aaron Gough <aaron@aarongough.com>
2010-06-06 02:25:12 -0400

    Added HTTP cacheing directives to controllers

--- app/controllers/gist_controller.rb
+++ app/controllers/gist_controller.rb
@@ -3,6 +3,8 @@ class GistController < ApplicationController
   # GET /gist/:id
   def index
     if(params[:id])
+      expires_in 30.minutes, :public => true, 'max-stale' => 10.minutes
+    
       @gist_markup = AjaxGist.gist_markup_for(params[:id].to_i)
       @id = params[:id].to_i
       
